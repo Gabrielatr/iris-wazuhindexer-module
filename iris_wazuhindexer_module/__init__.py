29094bd5e087b40e27bc0058829a855cc2420ba4
Gabrielatr <gabrielatr@ua.pt>
2026-07-23 10:43:23 +0100

    Initial commit

---
+++ iris_wazuhindexer_module/__init__.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+#
+#
+#  IRIS wazuhindexer Source Code
+#  Copyright (C) 2026 - iris-wazuhindexer-module
+#  hello@iris-wazuhindexer-module.com
+#  Created by iris-wazuhindexer-module - 2026-07-23
+#
+#  License MIT
+
+__iris_module_interface = "IrisWazuhindexerInterface"
---
+++ setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup
+
+setup(
+    name='iris-wazuhindexer-module',
+    python_requires='>=3.9',
+    version='0.1.0',
+    packages=['iris_wazuhindexer_module', 'iris_wazuhindexer_module.wazuhindexer_handler'],
+    url='https://github.com/iris_wazuhindexer_module/iris-wazuhindexer-module',
+    license='MIT',
+    author='iris-wazuhindexer-module',
+    author_email='hello@iris-wazuhindexer-module.com',
+    description='`iris-wazuhindexer-module` is a IRIS pipeline/processor module created with https://github.com/dfir-iris/iris-skeleton-module',
+    install_requires=[]
+)
