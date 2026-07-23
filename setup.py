0bd3e8179a841c621a434e94513f5fb1ac87da9a
Gabrielatr <gabrielatr@ua.pt>
2026-07-23 16:32:10 +0100

    Requirements

--- setup.py
+++ setup.py
@@ -7,8 +7,8 @@ setup(
     packages=['iris_wazuhindexer_module', 'iris_wazuhindexer_module.wazuhindexer_handler'],
     url='https://github.com/iris_wazuhindexer_module/iris-wazuhindexer-module',
     license='MIT',
-    author='iris-wazuhindexer-module',
+    author='Gabrielatr',
     author_email='hello@iris-wazuhindexer-module.com',
     description='`iris-wazuhindexer-module` is a IRIS pipeline/processor module created with https://github.com/dfir-iris/iris-skeleton-module',
-    install_requires=[]
+    install_requires=['opensearch-py>=2.0.0','pytz','requests','setuptools==59.6.0', 'iris-interface==1.2.0']
 )
